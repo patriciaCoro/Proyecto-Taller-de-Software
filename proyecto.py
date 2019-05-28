2e35d8c8c9fc8671d4be2117f7d3ae90f92e6198
Patricia Coronacion <patricia.coronacion@usil.pe>
2019-05-28 08:06:49 -0500

    Creación de la primera parte del juego

--- proyecto.py
+++ proyecto.py
@@ -1,4 +1,18 @@
-n= int(input("Ingrese numero de jugadores"))
+n= int(input("Ingrese numero de jugadores: "))
+J =[]
+NC =[]
+for i in range (n):
+    Integrante = input("Integrante" + str(i+1)+": ")
+    J.append(Integrante)
+    while True:
+        cartillas = int(input("Número de cartillas: "))
+        if (cartillas <= 3):
+            NC.append(cartillas)
+            break
+            
+print(NC)            
+
+    
 
 
 
