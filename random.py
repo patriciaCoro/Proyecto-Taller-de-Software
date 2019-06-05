9639c2416c8fa293a092627ec3bf8aa842f76656
Patricia Coronación <patricia.coronacion@usil.pe>
2019-06-05 08:09:10 -0500

    actualizacion random

--- random.py
+++ random.py
@@ -1,6 +1,18 @@
-from random import sample
-def num_bol():
-    num = list(range(1,81))
-    ele = random.sample(num,1)
-    
-    return(ele)
+import random
+bol_sac =[]
+
+c= 0
+num = list(range(1,81))
+random.shuffle(num)
+
+def mostrar_bolilla():
+    c= 0
+    resultado = 0
+    if c>80:
+        resultado = num[1]
+        bol_sac.append(resultado)
+        num.remove(resultado)
+        c=c+1
+    else:
+        resultado = "Ya no hay más bolillas "
+    return resultado
