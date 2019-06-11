5714f16879d1bfda836642074a09e996391a4117
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-11 08:41:15 -0500

    Depurando excepciones

--- Final2.py
+++ Final2.py
@@ -1,5 +1,5 @@
 import random
-n= int(input("Ingrese numero de jugadores: "))
+n= int(input("Ingrese número de jugadores: "))
 lista_integrantes =[]
 NumCartillas =[]
 for i in range (n):
@@ -36,31 +36,47 @@ pozo = suma*5
 
       
 def menu():
-    print ("Seleccione una opcion: ")
+    print ("Seleccione una opción: ")
     print ("\t1 - Mostrar  bolillas  ")
     print ("\t2 - Mostrar resumen de bolillas obtenidas ")
     print ("\t3 - Finalizar juego y mostrar el pozo ganado ")
     print ("\t4 - Reiniciar juego ")
 while True:
     menu()
-    menu_opciones = input("Ingrese un numero: ")
+    menu_opciones = input("Ingrese un número: ")
     if menu_opciones == "1":
         print("")
-        print("sacar bolillas")
+        print("Haz pulsado la opción Sacar bolillas")
         print (mostrar_bolilla())
     elif menu_opciones == "2":
         print("")
-        print("haz pulsado la opcion Mostrar resumen de bolillas")
+        print("Haz pulsado la opción Mostrar Resumen de bolillas")
         print (bol_sac)
     elif menu_opciones == "3":
         print("")
-        print("haz pulsado la opcion Finalizar juego, el pozo ganado es: ")
+        print("Haz pulsado la opción Finalizar juego, el pozo ganado es: ")
         print(pozo)
     elif menu_opciones == "4":
         print("")
-        print("haz pulsado la opcion Reiniciar juego ")
-        
+        print("Haz pulsado la opción Reiniciar juego ")
+        n= int(input("Ingrese número de jugadores: "))
+        lista_integrantes =[]
+        NumCartillas =[]
+        for i in range (n):
+            Integrante = input("Integrante" + str(i+1)+": ")
+            lista_integrantes.append(Integrante)
+            while True:
+                cartillas = int(input("Número de cartillas: "))
+                if (cartillas <= 3):
+                    NumCartillas.append(cartillas)
+                    break
+                else:
+                    print("Recuerde que solo puede commprar 3 cartillas")
 
+        bol_sac =[]
+
+        num = list(range(1,81))
+        random.shuffle(num)
     else:
         print("")
-        print("ingrese un numero valido")
+        print("Ingrese un número válido")
